28f14d3af83781dc154e68fa5d6542d64da91308
heenaI <hinaimran.mehr@gmail.com>
2025-03-14 13:10:16 +0100

    adding backend and fast api settings

--- CREATE DATABASE Liebkreis;.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE Liebkreis;
-CREATE USER scheduler_user WITH ENCRYPTED PASSWORD 'MeraPassword880677!';
-
-
-postgresql://hina:MeraPassword880677!@localhost/Liebkreis
-docker exec -it liebkreis_db psql -U hina -d Liebkreis
